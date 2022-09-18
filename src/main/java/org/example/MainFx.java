38986f96656d29fb2e469e5798b9557f6b1e8ab8
FGoecking <flaviogoecking@gmail.com>
2022-09-18 13:57:50 -0300

    criado estrura de telas

---
+++ src/main/java/org/example/MainFx.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainFx extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/TelaInicial.fxml"));
+            BorderPane root = fxmlLoader.load();
+            Scene scene = new Scene(root, 700,400);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
