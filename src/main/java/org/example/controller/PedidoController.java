415f74d04ba2920c29bea1625506f31d72529777
FGoecking <flaviogoecking@gmail.com>
2022-09-26 22:39:45 -0300

    Criação de telas e revisão de fluxo

--- src/main/java/org/example/controller/PedidoController.java
+++ src/main/java/org/example/controller/PedidoController.java
@@ -3,24 +3,25 @@ package org.example.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.example.DAO.PedidoDAO;
 import org.example.model.PedidoModel;
 
-import java.util.List;
-
 public class PedidoController{
 
+
     @FXML
-    private TextArea inputTexto;
+    private BorderPane listagemPedidos;
+
+
+
 
     private PedidoDAO pedidoDAO = new PedidoDAO();
 
-    public void abreTelaNovoPedido(Stage stage) throws Exception {
+    public void abreTelaNovoPedido(Stage stage) {
         try{
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/CadastroPedido.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/ListagemPedidos.fxml"));
             BorderPane root = fxmlLoader.load();
             Scene scene = new Scene(root, 700,400);
 
@@ -31,10 +32,25 @@ public class PedidoController{
         }
     }
 
-        @FXML
-    private void cadastrarPedido(){
+    public void abrirCadastroPedido() {
+
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/CadastroPedido.fxml"));
+            BorderPane root = fxmlLoader.load();
+            Stage listagemPedidosStage = (Stage) this.listagemPedidos.getScene().getWindow();
+            Scene scene = new Scene(root, 700,400);
+
+            listagemPedidosStage.setScene(scene);
+            listagemPedidosStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void cadastrarPedido() {
+
         PedidoModel pedidoModel = new PedidoModel();
-        pedidoModel.setObservacao(inputTexto.getText());
         System.out.println(pedidoModel.getObservacao());
         pedidoDAO.salvarPedido(pedidoModel);
     }
