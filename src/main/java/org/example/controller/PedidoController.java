883181a16eed3d6a581fd62f2ca29611fc8808a2
FGoecking <flaviogoecking@gmail.com>
2022-09-26 23:07:55 -0300

    Botões para voltar para tela anterior

--- src/main/java/org/example/controller/PedidoController.java
+++ src/main/java/org/example/controller/PedidoController.java
@@ -2,22 +2,55 @@ package org.example.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.example.DAO.PedidoDAO;
 import org.example.model.PedidoModel;
 
-public class PedidoController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PedidoController implements Initializable {
 
 
     @FXML
     private BorderPane listagemPedidos;
+    @FXML
+    private BorderPane cadastroPedido;
+    @FXML
+    private TableView<PedidoModel> tblPedidos;
+    @FXML
+    private TableColumn<PedidoModel, String> colCliente;
+    @FXML
+    private TableColumn<PedidoModel, String> colCabecote;
+    @FXML
+    private TableColumn<PedidoModel, String> colDatEntrega;
+    @FXML
+    private TableColumn<PedidoModel, String> colValor;
 
+    private final PedidoDAO pedidoDAO = new PedidoDAO();
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        /*colCliente.setCellValueFactory( Pedido -> {
+            SimpleObjectProperty property = new SimpleObjectProperty();
+            property.setValue(Pedido.getValue().getCliente().getNome());
+            return property;
+        });
+        colCliente.setCellValueFactory( Pedido -> {
+            SimpleObjectProperty property = new SimpleObjectProperty();
+            property.setValue(Pedido.getValue().getCabecote().getNome());
+            return property;
+        });
+        colDatEntrega.setCellValueFactory(new PropertyValueFactory<>("datOrcamento"));
+        colDatEntrega.setCellValueFactory(new PropertyValueFactory<>("totalGeral"));
 
-
-    private PedidoDAO pedidoDAO = new PedidoDAO();
+        tblPedidos.setItems(pedidoDAO.buscarListagemPedido());*/
+    }
 
     public void abreTelaNovoPedido(Stage stage) {
         try{
@@ -32,8 +65,23 @@ public class PedidoController{
         }
     }
 
-    public void abrirCadastroPedido() {
+    public void voltarTelaInicial() {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/TelaInicial.fxml"));
+            BorderPane root = fxmlLoader.load();
+            Stage listagemPedidosStage = (Stage) this.listagemPedidos.getScene().getWindow();
+            Scene scene = new Scene(root, 700,400);
 
+            listagemPedidosStage.setScene(scene);
+            listagemPedidosStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void abrirCadastroPedido() {
+        System.out.println("Cadastro pedido");
         try{
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/CadastroPedido.fxml"));
             BorderPane root = fxmlLoader.load();
@@ -46,6 +94,20 @@ public class PedidoController{
             e.printStackTrace();
         }
 
+    }
+
+    public void voltarListagemPedidos() {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/ListagemPedidos.fxml"));
+            BorderPane root = fxmlLoader.load();
+            Stage cadastroPedidosStage = (Stage) this.cadastroPedido.getScene().getWindow();
+            Scene scene = new Scene(root, 700,400);
+
+            cadastroPedidosStage.setScene(scene);
+            cadastroPedidosStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void cadastrarPedido() {
--- src/main/java/org/example/controller/TelaInicialController.java
+++ src/main/java/org/example/controller/TelaInicialController.java
@@ -2,50 +2,18 @@ package org.example.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import org.example.DAO.PedidoDAO;
-import org.example.model.PedidoModel;
 
 
 public class TelaInicialController {
 
     @FXML
     private BorderPane telaInicial;
-    @FXML
-    private TableView<PedidoModel> tblPedidos;
-    @FXML
-    private TableColumn<PedidoModel, String> colCliente;
-    @FXML
-    private TableColumn<PedidoModel, String> colCabecote;
-    @FXML
-    private TableColumn<PedidoModel, String> colDatEntrega;
-    @FXML
-    private TableColumn<PedidoModel, String> colValor;
 
-    PedidoDAO pedidoDAO = new PedidoDAO();
+    private final PedidoController pedidoController = new PedidoController();
 
-    private PedidoController pedidoController = new PedidoController();
-
-   /* @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        colCliente.setCellValueFactory( Pedido -> {
-            SimpleObjectProperty property = new SimpleObjectProperty();
-            property.setValue(Pedido.getValue().getCliente().getNome());
-            return property;
-        });
-        colCliente.setCellValueFactory( Pedido -> {
-            SimpleObjectProperty property = new SimpleObjectProperty();
-            property.setValue(Pedido.getValue().getCabecote().getNome());
-            return property;
-        });
-        colDatEntrega.setCellValueFactory(new PropertyValueFactory<>("datOrcamento"));
-        colDatEntrega.setCellValueFactory(new PropertyValueFactory<>("totalGeral"));
-
-        tblPedidos.setItems(pedidoDAO.buscarListagemPedido());
-    }
+   /*
 
     private ObservableList<PedidoModel> criaMockPedidos(){
         ClienteModel clienteModel = new ClienteModel();
@@ -58,8 +26,8 @@ public class TelaInicialController {
         return FXCollections.observableArrayList(pedidoModel, pedidoModel);
     }*/
 
-    public void listagemPedidos(ActionEvent actionEvent) throws Exception {
-        System.out.println("Criando pedido");
+    public void listagemPedidos() {
+        System.out.println("Listagem pedido");
 
         Stage telaInicialStage = (Stage) this.telaInicial.getScene().getWindow();
         pedidoController.abreTelaNovoPedido(telaInicialStage);
