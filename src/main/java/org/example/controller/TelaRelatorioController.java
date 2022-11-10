bff08de10a0119cd4ba9ade681488d15f1982059
dragonger <miguelbelizario144@gmail.com>
2022-11-10 08:35:32 -0300

    Controller Tela relatorio

---
+++ src/main/java/org/example/controller/TelaRelatorioController.java
@@ -0,0 +1,37 @@
+package org.example.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class TelaRelatorioController {
+    public BorderPane TelaRelatorio;
+
+    public void abreTelaRelatorio(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/TelaRelatorio.fxml"));
+            BorderPane root = fxmlLoader.load();
+            Scene scene = new Scene(root, 700, 400);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void voltarTelaInicial() {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/TelaInicial.fxml"));
+            BorderPane root = fxmlLoader.load();
+            Stage telaRelatorio = (Stage) this.TelaRelatorio.getScene().getWindow();
+            Scene scene = new Scene(root, 700,400);
+
+            telaRelatorio.setScene(scene);
+            telaRelatorio.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
